ae9a6e7c29c89c65208bafcfdc625db9a245262e
khanh <khanhlnfx20736@funix.edu.vn>
2024-09-10 22:24:58 +0700

    edit

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -5,7 +5,12 @@ function Type() {
   return (
     <Typewriter
       options={{
-        strings: ["Front-end Developer", "Back-end Developer", "Freelancer"],
+        strings: [
+          "Front-end Developer",
+          "Back-end Developer",
+          "Freelancer",
+          "Mobile Developer",
+        ],
         autoStart: true,
         loop: true,
         deleteSpeed: 50,
